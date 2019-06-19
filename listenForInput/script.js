0fc68eda31f6aec9ceb720046984e838caddc84d
Alican <alican.yildirim@ceng.metu.edu.tr>
2019-06-19 23:02:42 +0300

    added a form to listenForInput

--- listenForInput/script.js
+++ listenForInput/script.js
@@ -1,20 +1,36 @@
 if(!window.webkitSpeechRecognition){
       log('Sorry this will work only in Chrome for now...');
 }
+var message = document.querySelector('#message');
 const magic_word = 'Hello';
-var times = 0;
-let recognition = new webkitSpeechRecognition();
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar,1);
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 recognition.continuous = true;
-
 recognition.onresult = e => {
     var transcripts  = [].concat.apply([], [...e.results].map(res => [...res].map(alt => alt.transcript)));
     if(transcripts.some(t=>t.indexOf(magic_word)>-1)){
         log('!!!do something awesome!!!');
         times++;
         awesome.textContent = 'I heard you say Hello ' + times + ' times.';
+        var last = event.results.length -1;
+        var command = event.results[last][0].transcript;
+        message.textContent = 'Voice Input: ' + command + '.';
+        if(command.toLowerCase() === "select male")
+        {
+            document.querySelector('#male').checked = true;
+        }
+        else if(command.toLowerCase() === "select female")
+        {
+            document.querySelector('#female').checked = true;
+        }
+        else if (command.toLowerCase() === "select other")
+        {
+            document.querySelector('#other').checked = true;
+        }
     }
     else{
         log('understood ' + JSON.stringify(transcripts));
