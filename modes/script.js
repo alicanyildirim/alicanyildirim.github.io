f506f40e182cc13e3f61e71ec8321beaee8b6c5c
Alican <alican.yildirim@ceng.metu.edu.tr>
2019-06-20 16:22:09 +0300

    added some css, and mode.

---
+++ modes/script.js
@@ -0,0 +1,91 @@
+var message = document.querySelector('#message');
+var confidence = document.querySelector('#confidence');
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var grammar = '#JSGF V1.0;'
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+var confidencePercent;
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.lang = 'en-US';
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.onresult = function(event) {
+    var last = event.results.length - 1;
+    var command = event.results[last][0].transcript;
+    command = command.toLowerCase();
+    message.textContent = 'Voice Input: ' + command + '.';
+    confidencePercent = (event.results[0][0].confidence * 100);
+    confidence.textContent = 'Confidence: %' + confidencePercent.toFixed(2);
+    inputMode();
+  recognition.start();
+};
+function navigationMode(command)
+{
+    if(command === 'next')
+    {
+        document.querySelector('.next').click();
+    }
+    else if (command === 'previous')
+    {
+        document.querySelector('.previous').click();
+    }
+    else if (command === 'enter input mode')
+    {
+        // change the mode to the input mode.
+        // by focusing on the form
+    }
+}
+function inputMode(command)
+{
+    if(command === 'enter navitagiton mode')
+    {
+        //change the mode to the navigation mode
+
+    }
+    else if (command === 'select steve'){
+        document.querySelector('#chkSteve').checked = true;
+    }
+    else if (command === 'select tony'){
+        document.querySelector('#chkTony').checked = true;
+    }
+    else if (command === 'select bruce'){
+        document.querySelector('#chkBruce').checked = true;
+    }
+    else if (command === 'select nick'){
+        document.querySelector('#chkNick').checked = true;
+    }
+    else if(command === 'deselect steve'){
+        document.querySelector('#chkSteve').checked = false;
+    }
+    else if (command === 'deselect tony'){
+        document.querySelector('#chkTony').checked = false;
+    }
+    else if (command === 'deselect bruce'){
+        document.querySelector('#chkBruce').checked = false;
+    }
+    else if (command === 'deselect nick'){
+        document.querySelector('#chkNick').checked = false;
+    }
+    else
+    {
+        var focus = document.activeElement;
+        focus.value=command;
+        //get the element that has focus
+        //the input will be given to that element.
+    }
+
+}
+recognition.onspeechend = function() {
+    recognition.start();
+};
+recognition.onerror = function(event) {
+    message.textContent = 'Error occurred in recognition: ' + event.error;
+}
+recognition.onend = () => recognition.start();
+
+
+window.onload = (event) => {
+    recognition.start();
+};
