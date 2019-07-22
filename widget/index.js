8ab6cefa94c39564634787ee67867eb1611cc2cf
Alican <alican.yildirim@ceng.metu.edu.tr>
2019-07-22 15:00:31 +0300

    move one by one god dammit

--- widget/index.js
+++ widget/index.js
@@ -248,6 +248,7 @@ recognition.onresult = function(event) {
 
 }
 
+recognition.onspeechend = () => recognition.start();
 
 recognition.onerror = (event) => message.textContent = 'Error occurred in recognition: ' + event.error;
 
